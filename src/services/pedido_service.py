d51451065ede80f34d2097b986bd93aed4c50da0
RAFAEL SIMÕES CRUSOÉ <1T1.912296@universidadesenaicimatec.edu.br>
2026-04-16 13:59:21 -0300

    Arquitetura em camadas: implementação da camada de serviços.

---
+++ src/services/pedido_service.py
@@ -0,0 +1,15 @@
+from src.models.pedido import Pedido
+
+class PedidoService:
+    """Classe de serviço para processar pedidos e aplicar descontos."""
+
+    def __init__(self):
+        self.pedidos = []
+
+    def adicionar_pedido(self, pedido: Pedido):
+        self.pedidos.append(pedido)
+
+    def processar_pedidos(self):
+        for pedido in self.pedidos:
+            print(f"Cliente: {pedido.cliente}")
+            print(f"Valor final: {pedido.valor_final(pedido.valor_original)}")
