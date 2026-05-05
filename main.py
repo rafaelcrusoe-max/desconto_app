b06bea1ecda51c8f09fcf1f5f99a3c0553ee7944
RAFAEL SIMÕES CRUSOÉ <1T1.912296@universidadesenaicimatec.edu.br>
2026-05-05 14:43:56 -0300

    Arquitetura limpa: Implementação do fluxo até camada de controller e chamada na main

--- main.py
+++ main.py
@@ -1,25 +1,62 @@
-from src.controllers.pedido_controller import PedidoController
-from src.repositories.pedido_repository import PedidoRepository
-from src.services.pedido_service import PedidoService
-from src.database.connection import DatabaseConnection
-from src.models.pedido import Pedido
-from src.models.desconto import DescontoNormal, DescontoVIP, DescontoPremium
+from src.app.frameworks.database.memory_database import MemoryDatabase
+from src.app.use_cases.criar_pedido import CriarPedido
+from src.app.adapters.controllers.pedido_controller import PedidoController
+from src.app.adapters.repositories.memory_pedido_repository import memoryPedidoRepository
 
-if __name__ == "__main__":
-    database = DatabaseConnection()
-    repo = PedidoRepository(database)
-    service = PedidoService(repo)
-    controller = PedidoController(service)
+def main() -> None:
+    database = MemoryDatabase()
+    pedido_gateway = memoryPedidoRepository(database)
+    criar_pedido_use_case = CriarPedido(pedido_gateway)
+    controller = PedidoController(criar_pedido_use_case)
 
-    pedido1 = Pedido("Cliente 1", DescontoNormal())
-    pedido1.valor_original = 100.0
-    pedido2 = Pedido("Cliente 2", DescontoVIP())
-    pedido2.valor_original = 100.0
-    pedido3 = Pedido("Cliente 3", DescontoPremium())
-    pedido3.valor_original = 100.0
+    pedido1 = controller.criar_pedido("Cliente A", 100, "normal")
+    pedido2 = controller.criar_pedido("Cliente B", 100, "vip")
+    pedido3 = controller.criar_pedido("Cliente C", 100, "premium")
 
-    controller.adicionar_pedido(pedido1)
-    controller.adicionar_pedido(pedido2)
-    controller.adicionar_pedido(pedido3)
+    print("Pedidos criados:")
+    print(pedido1.cliente, pedido1.valor_original, pedido1.valor_final())
+    print(pedido2.cliente, pedido2.valor_original, pedido2.valor_final())
+    print(pedido3.cliente, pedido3.valor_original, pedido3.valor_final())
 
-    controller.processar_pedidos()
+    if __name__== "__main__":
+        main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from src.controllers.pedido_controller import PedidoController
+# from src.repositories.pedido_repository import PedidoRepository
+# from src.services.pedido_service import PedidoService
+# from src.database.connection import DatabaseConnection
+# from src.models.pedido import Pedido
+# from src.models.desconto import DescontoNormal, DescontoVIP, DescontoPremium
+
+# if __name__ == "__main__":
+#     database = DatabaseConnection()
+#     repo = PedidoRepository(database)
+#     service = PedidoService(repo)
+#     controller = PedidoController(service)
+
+#     pedido1 = Pedido("Cliente 1", DescontoNormal())
+#     pedido1.valor_original = 100.0
+#     pedido2 = Pedido("Cliente 2", DescontoVIP())
+#     pedido2.valor_original = 100.0
+#     pedido3 = Pedido("Cliente 3", DescontoPremium())
+#     pedido3.valor_original = 100.0
+
+#     controller.adicionar_pedido(pedido1)
+#     controller.adicionar_pedido(pedido2)
+#     controller.adicionar_pedido(pedido3)
+
+#     controller.processar_pedidos()
