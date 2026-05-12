03f1ddc838c8d21b84c8d3c51d0d8267dec584e8
RAFAEL SIMÕES CRUSOÉ <1T1.912296@universidadesenaicimatec.edu.br>
2026-05-12 14:43:26 -0300

    Implementação de todo fluxo e chamada na main

--- src/app/adapters/controllers/pedido_controller.py
+++ src/app/adapters/controllers/pedido_controller.py
@@ -1,6 +1,6 @@
 from src.app.use_cases.criar_pedido import CriarPedido
 from src.app.dtos.criar_pedido_input_dto import CriarPedidoInputDTO
-from src.app.presenters.pedido_presenter import PedidoPresenter
+from src.app.presenters.pedido_presenters import PedidoPresenter
 
 
 class PedidoController:
